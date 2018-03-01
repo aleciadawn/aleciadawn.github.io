21e69e073b0dee2107aee820a008cc5728a95213
Alecia McMahon <alecia@aleciadawn.co>
2018-03-01 15:07:25 -0700

    Connecting JavaScript stylesheet

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -1,8 +1,22 @@
-var myElement = $('.hero-content');
+/*$(window).scroll(function(){
+    $(".hero-title").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
 
-$(window).on('scroll', function() {
-    var st = $(this).scrollTop();
-    myElement.css({
-        'opacity' : 1 - st/600
-    });
-});
+/*win.scroll(function(){
+  scrollPosition = win.scrollTop();
+  scrollRatio = 1 - scrollPosition / 300;
+  $(".top").css("opacity", scrollRatio);
+*/
+
+//var controller = new ScrollMagic.Controller();
+
+// create a scene
+//new ScrollMagic.Scene({
+		//duration: 100,	// the scne should last for a scroll distance of 100px
+		//offset: 50		// start this scene after scrolling for 50px
+	//})
+	//.setPin("#hero-content" // pins the element for the the scene's duration
+	//.addTo(controller);// // assign the scene to the controlle
+
+
+console.log("hi")
