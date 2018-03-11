d776aaac95864e3f54dff7060973f18ac1246abb
Alecia McMahon <alecia@aleciadawn.co>
2018-03-10 20:27:28 -0700

    Clean up unfinished HP case study page

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -1,32 +1,31 @@
-
 var myElement = document.getElementById('hero-content');
 
 window.onscroll = function() {fader()};
 
 function fader() {
-  var st = document.body.scrollTop;
-  console.log(st);
+  var st = window.pageYOffset;
   myElement.style.opacity = 1 - st/300;
 }
 
-      var tBox = document.getElementById('hero-content');
-			var lBox1 = document.getElementById('about-me-container');
-			var rBox1 = document.getElementById('photo-container');
 
-			window.onscroll = function() {scrollAnimations()};
 
-			function scrollAnimations() {
-				var ypos = document.body.scrollTop;
-				var wh = window.innerHeight;
-
-				var pScroll = ypos/wh;
-				tBox.style.opacity = 1 - pScroll;
-
-				var offper = 100 - ( 100 * pScroll);
-
-				if( pScroll > 1 ) {
-					var offper = 0;
-				}
-				lBox1.style.transform = "translateX(-" + offper + "%)";
-				rBox1.style.transform = "translateX(" + offper + "%)";
-			}
+			// window.onscroll = function() {scrollAnimations()};
+      //
+			// function scrollAnimations() {
+      //   var tBox = document.getElementById('hero-content');
+      //   var lBox1 = document.getElementById('about-me-container');
+      //   var rBox1 = document.getElementById('photo-container');
+			// 	var ypos = window.pageYOffset;
+			// 	var wh = window.innerHeight;
+      //   var pScroll = ypos/wh;
+      //
+			// 	tBox.style.opacity = 1 - pScroll;
+      //
+			// 	var offper = 100 - ( 100 * pScroll);
+      //
+			// 	if( pScroll > 1 ) {
+			// 		var offper = 0;
+			// 	}
+			// 	lBox1.style.transform = "translateX(-" + offper + "%)";
+			// 	rBox1.style.transform = "translateX(" + offper + "%)";
+			// }
