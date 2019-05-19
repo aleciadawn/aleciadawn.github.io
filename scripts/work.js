ef53040614deadfb24e56dc16bed3b7dc2ae1efd
Alecia Dawn <34486061+aleciadawn@users.noreply.github.com>
2019-05-18 21:48:02 -0600

    Update work.js

--- scripts/work.js
+++ scripts/work.js
@@ -1,22 +1,22 @@
-$(function (){
-  var controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave'
-    }
-  });
+// $(function (){
+//   var controller = new ScrollMagic.Controller({
+//     globalSceneOptions: {
+//       triggerHook: 'onLeave'
+//     }
+//   });
 
-  var slaides = document.querySelectorAll ("section.panel");
+//   var slaides = document.querySelectorAll ("section.panel");
 
-  for (var i=0; i<slides.length; i++) {
-    new ScrollMagic.Scene({
-        triggerElement: slides [i]
-        })
-        .setPin(slides[i])
-        .addIndicators()
-        .addTo(controller);
-  });
-
-
+//   for (var i=0; i<slides.length; i++) {
+//     new ScrollMagic.Scene({
+//         triggerElement: slides [i]
+//         })
+//         .setPin(slides[i])
+//         .addIndicators()
+//         .addTo(controller);
+//   });
 
 
-console.log("hi")
+
+
+// console.log("hi")
