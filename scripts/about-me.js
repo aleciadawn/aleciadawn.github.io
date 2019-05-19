3836218e21f15bb38a2bc3afb43bc385f8d7fc10
Alecia Dawn <34486061+aleciadawn@users.noreply.github.com>
2019-05-18 21:48:24 -0600

    Update about-me.js

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -1,63 +1,63 @@
-var myElement = document.getElementById('hero');
+// var myElement = document.getElementById('hero');
 
-window.onscroll = function() {fader()};
+// window.onscroll = function() {fader()};
 
-function fader() {
-  var st = window.pageYOffset;
-  myElement.style.opacity = 1 - st/300;
-}
-
-
-
-			window.onscroll = function() {scrollAnimations()};
-
-			function scrollAnimations() {
-        var tBox = document.getElementById('hero');
-        var lBox1 = document.getElementById('about-me-container');
-        var rBox1 = document.getElementById('photo-container');
-				var ypos = window.pageYOffset;
-				var wh = window.innerHeight;
-        var pScroll = ypos/wh;
-
-				tBox.style.opacity = 1 - pScroll;
-
-				var offper = 100 - ( 100 * pScroll);
-
-				if( pScroll > 1 ) {
-					var offper = 0;
-				}
-				lBox1.style.transform = "translateX(-" + offper + "%)";
-				rBox1.style.transform = "translateX(" + offper + "%)";
-			}
+// function fader() {
+//   var st = window.pageYOffset;
+//   myElement.style.opacity = 1 - st/300;
+// }
 
 
 
-  $(document).ready(function() {
+// 			window.onscroll = function() {scrollAnimations()};
 
-  var scrollLink = $('.scroll');
+// 			function scrollAnimations() {
+//         var tBox = document.getElementById('hero');
+//         var lBox1 = document.getElementById('about-me-container');
+//         var rBox1 = document.getElementById('photo-container');
+// 				var ypos = window.pageYOffset;
+// 				var wh = window.innerHeight;
+//         var pScroll = ypos/wh;
 
-  // Smooth scrolling //
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
-  });
+// 				tBox.style.opacity = 1 - pScroll;
 
-  // Active link switching
-  $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
+// 				var offper = 100 - ( 100 * pScroll);
 
-    scrollLink.each(function() {
+// 				if( pScroll > 1 ) {
+// 					var offper = 0;
+// 				}
+// 				lBox1.style.transform = "translateX(-" + offper + "%)";
+// 				rBox1.style.transform = "translateX(" + offper + "%)";
+// 			}
 
-      var sectionOffset = $(this.hash).offset().top - 20;
 
-      if ( sectionOffset <= scrollbarLocation ) {
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
-      }
-    })
 
-  })
+//   $(document).ready(function() {
 
-})
+//   var scrollLink = $('.scroll');
+
+//   // Smooth scrolling //
+//   scrollLink.click(function(e) {
+//     e.preventDefault();
+//     $('body,html').animate({
+//       scrollTop: $(this.hash).offset().top
+//     }, 1000 );
+//   });
+
+//   // Active link switching
+//   $(window).scroll(function() {
+//     var scrollbarLocation = $(this).scrollTop();
+
+//     scrollLink.each(function() {
+
+//       var sectionOffset = $(this.hash).offset().top - 20;
+
+//       if ( sectionOffset <= scrollbarLocation ) {
+//         $(this).parent().addClass('active');
+//         $(this).parent().siblings().removeClass('active');
+//       }
+//     })
+
+//   })
+
+// })
